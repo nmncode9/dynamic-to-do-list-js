090c26291a8dd93c702c0ea367dfb1671e9cb5df
numan <197640394+nmncode9@users.noreply.github.com>
2025-06-15 21:37:40 +0100

    Create addTask function and event handlers

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
 
+    function addTask() {
+
+        const taskText = taskInput.value.trim()
+
+        if (!taskText) {
+            alert("Enter a task")
+        } else {
+            const task = document.createElement("li");
+            task.textContent = taskText;
+            
+            const removeButton = document.createElement("button");
+            removeButton.textContent = "Remove";
+            removeButton.classList.add("remove-btn");
+            removeButton.addEventListener("click", () => task.remove());
+            
+            task.appendChild(removeButton);
+            taskList.appendChild(task);
+
+            taskInput.value = "";
+        }
+
+    }
+
+    addButton.addEventListener("click", addTask)
+
+});
